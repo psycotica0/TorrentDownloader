ad923dfef4caed8a8b5978d9620a59e01ef580a2
Christopher Vollick <psycotica0@gmail.com>
2011-07-17 00:51:29 -0400

    Actually Downloading
    This seems super crazy and probably fragile.
    Seems to work, though.
    
    So... essentially done...

--- download.rb
+++ download.rb
@@ -1,5 +1,7 @@
 require 'mustache'
 
+require 'open-uri'
+
 class TorrentDownload < Mustache
 	attr_reader :title, :name, :type
 
@@ -8,6 +10,20 @@ class TorrentDownload < Mustache
 		@name = @env.POST["name"]
 		@title = "Download for " + @name + " Queued"
 		@type = @env.POST["type"]
+
+		download_file
+	end
+
+	def get_dir(type)
+		{"movie" => ".", "tv" => "."}[type]
+	end
+
+	def download_file
+		open(get_dir(@type) + "/" + filename, 'w') { |output_file|
+			open(URI.escape(@env.POST["enclosure_url"])) { |input_file|
+				output_file.write(input_file.read)
+			}
+		}
 	end
 
 	def filename
